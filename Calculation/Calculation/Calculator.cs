6cb5d666dbffcbb3c4e571bb13f0b94430b17504
hvns08 <52692404+hvns08@users.noreply.github.com>
2019-11-27 16:50:16 +0400

    Add files via upload

---
+++ Calculation/Calculation/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculation
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public double AddDouble(double a,double b)
+        {
+            return a + b;
+        }
+    }
+}
